9897605b4260803daf4e9d80ae3220ca07887264
Roland Hesse <rolandhesse11783@gmail.com>
2023-08-14 16:24:18 +0200

    solve pagination and the-search-bar task

--- index.js
+++ index.js
@@ -10,14 +10,16 @@ const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
 // States
-const maxPage = 1;
-const page = 1;
-const searchQuery = "";
+const maxPage = 42;
+let page = 1;
+let searchQuery = "";
+
+//let data;
 
 async function fetchCharacters() {
   cardContainer.innerHTML = "";
   const response = await fetch(
-    "https://rickandmortyapi.com/api/character/?page=1"
+    `https://rickandmortyapi.com/api/character/?page=${page}&name=${searchQuery}`
   );
   const data = await response.json();
   const allCharacters = data.results;
@@ -25,6 +27,33 @@ async function fetchCharacters() {
     const newCharacterCard = createCharacterCard(character);
     cardContainer.append(newCharacterCard);
   });
+  //console.log(data);
 }
 
 fetchCharacters();
+
+//Task 3: Pagination
+nextButton.addEventListener("click", () => {
+  if (page < 42 && page >= 1) {
+    page += 1;
+    pagination.textContent = `${page}/${maxPage}`;
+    fetchCharacters(page);
+  }
+});
+
+prevButton.addEventListener("click", () => {
+  if (page <= 42 && page > 1) {
+    page -= 1;
+    pagination.textContent = `${page}/${maxPage}`;
+    fetchCharacters(page);
+  }
+});
+
+//Task 4: The Search Bar
+searchBar.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchQuery = event.target[0].value;
+  // console.log("event.target[0].value: ", event.target[0].value);
+  // console.log("event: ", event);
+  fetchCharacters();
+});
