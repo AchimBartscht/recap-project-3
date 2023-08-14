361a259ef37de4e6acac973857b001c70a6a247f
Achim Bartscht <beemerhh@gmail.com>
2023-08-14 11:17:10 +0200

    change-character-names

--- components/card/card.js
+++ components/card/card.js
@@ -1,28 +1,27 @@
 const cardContainer = document.querySelector('[data-js="card-container"]');
-export function createCharacterCard() {
+export function createCharacterCard(character) {
   const newCard = document.createElement("li");
   newCard.classList.add("card");
-  const imageSource = imageUrl;
-  const characterName =
-    (newCard.innerHTML = `<div class="card__image-container">
+
+  newCard.innerHTML = `<div class="card__image-container">
     <img
       class="card__image"
-      src="${imageUrl}"
-      alt="${characterName}"
+      src="${character.image}"
+      alt="${character.name}"
     />
     <div class="card__image-gradient"></div>
   </div>
   <div class="card__content">
-    <h2 class="card__title">${characterName}</h2>
+    <h2 class="card__title">${character.name}</h2>
     <dl class="card__info">
       <dt class="card__info-title">Status</dt>
-      <dd class="card__info-description">${characterStatus}</dd>
+      <dd class="card__info-description">${character.status}</dd>
       <dt class="card__info-title">Type</dt>
-      <dd class="card__info-description">${characterType}</dd>
+      <dd class="card__info-description">${character.type}</dd>
       <dt class="card__info-title">Occurrences</dt>
-      <dd class="card__info-description">${characterOccurrences}</dd>
+      <dd class="card__info-description">${character.episode.length}</dd>
     </dl>
-  </div>`);
+  </div>`;
   cardContainer.append(newCard);
 }
 createCharacterCard();
